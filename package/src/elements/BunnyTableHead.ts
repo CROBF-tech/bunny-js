81c9d5697432d540ada6b93fa3c96f3135efcde5
juan beresiarte <juanberesiarte@gmail.com>
2025-04-02 14:45:49 -0300

    Config package for npm

--- package/src/elements/BunnyTableHead.ts
+++ package/src/elements/BunnyTableHead.ts
@@ -2,7 +2,7 @@ import BunnyElement from "./BunnyElement";
 /**
  * Clase especializada para celdas de encabezado de tabla
  */
-export default class BunnyTableHead extends BunnyElement<HTMLTableHeaderCellElement> {
+export default class BunnyTableHead extends BunnyElement<HTMLTableCellElement> {
   constructor() {
     super("th");
   }
---
+++ package/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"), // Punto de entrada
+      name: "bunny",
+      fileName: (format) => `bunny.${format}.js`,
+      formats: ["es", "umd"], // Genera ESM y UMD
+    },
+    minify: "esbuild",
+    rollupOptions: {
+      output: [
+        {
+          format: "es",
+          entryFileNames: "bunny.es.js",
+        },
+        {
+          format: "umd",
+          entryFileNames: "bunny.umd.js",
+          name: "bunny",
+        },
+      ],
+    },
+  },
+});
